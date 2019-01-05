d00c6da292ecabc3d88f4c927753aa17bfb160ec
Alexey Zakharov <embalexembalex@Alexeys-MacBook-Pro.local>
2019-01-05 13:17:15 +0300

    Update config for tests. Add mock for worker

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,22 @@
-module.exports = function override(config, env) {
-  config.output.globalObject = 'this';
-  return config;
+module.exports = {
+  webpack: function (config) {
+    config.output.globalObject = 'this';
+    return config;
+  },
+  jest: function (config) {
+    config.moduleNameMapper = {
+      '\\.worker.entry.js': '<rootDir>/__mocks__/workerMock.js',
+    };
+    config.collectCoverageFrom = [
+      'src/**/*.{js,jsx}',
+    ];
+    config.snapshotSerializers = [
+      '<rootDir>/node_modules/enzyme-to-json/serializer',
+    ];
+
+    return config;
+  },
+  devServer: function (configFunction) {
+    return configFunction;
+  },
 };
