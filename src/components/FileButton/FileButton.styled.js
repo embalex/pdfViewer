f5c9a154e73f5f44ad5b23f20062964cc2222205
Alexey Zakharov <embalexembalex@Alexeys-MacBook-Pro.local>
2019-01-05 22:26:17 +0300

    Update react-pdf. Update style of FileButton

--- src/components/FileButton/FileButton.styled.js
+++ src/components/FileButton/FileButton.styled.js
@@ -2,11 +2,12 @@ import styled from 'styled-components';
 
 
 export const Button = styled.button`
-  border-radius: 5px;
-  background: lightgreen;
-  box-shadow: 2px 2px black;
+height: 40px;
+  background: #ffffff;
+  box-shadow: 0px 3px 4px rgba(0, 0, 0, 0.13), 0px 1px 3px rgba(0, 0, 0, 0.1);
+  border-radius: 3px;
   padding: 5px 20px;
   :focus {
     outline: none;
-}
+  }
 `;
--- src/stories/pdfPreview.stories.js
+++ src/stories/pdfPreview.stories.js
@@ -5,5 +5,5 @@ import { storiesOf } from '@storybook/react';
 import Demo from '../components/Demo';
 
 
-storiesOf('App ', module)
-  .add('basic story', () => <Demo />);
+storiesOf('PDF preview ', module)
+  .add('main', () => <Demo />);
